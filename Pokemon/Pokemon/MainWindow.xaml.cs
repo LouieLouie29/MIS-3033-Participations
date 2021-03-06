c7acee196a8baf8f3d23ba87e934719aaed9fdef
Edward Louie <edwardlouie29@ou.edu>
2021-03-05 22:33:02 -0600

    Pokemon JSON

---
+++ Pokemon/Pokemon/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pokemon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PokemonAPI api;
+            string url = "https://pokeapi.co/api/v2/pokemon?limit=1200";
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url).Result;
+
+                api = JsonConvert.DeserializeObject<PokemonAPI>(json);
+            }
+
+            foreach (var item in api.results.OrderBy(x => x.name).ToList())
+            {
+                lstPokemon.Items.Add(item);                
+            }
+
+            foreach (var item in api.results.OrderBy(x => x.height).ToList())
+            {
+                lstHeight.Items.Add(item);
+            }
+            /*
+            foreach (var item in api.results.OrderBy(a => a.height.ToList())
+            {
+
+            }
+            */
+        }
+    }
+}
---
+++ Pokemon/Pokemon/PokemonAPI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokemon
+{
+    public class PokemonAPI
+    {
+        public List<ResultObject> results { get; set; }
+
+    }
+
+    public class ResultObject
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        
+        public int height { get; set; }
+
+        public double weight { get; set; }
+
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+
+
+}
